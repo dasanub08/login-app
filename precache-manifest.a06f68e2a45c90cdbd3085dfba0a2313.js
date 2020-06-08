478c57a542578b9e2f860a84efdf78a41ec2cc3d
dasanub_08 <anubrata.life08@gmail.com>
2020-06-08 23:03:12 +0530

    Updates

---
+++ precache-manifest.a06f68e2a45c90cdbd3085dfba0a2313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fc6f3ce6cbabf995e6c4a265a1a241",
+    "url": "/login-app/index.html"
+  },
+  {
+    "revision": "9754714cfc7c6101334a",
+    "url": "/login-app/static/css/main.318f65ee.chunk.css"
+  },
+  {
+    "revision": "9dedc5387793664a2ce4",
+    "url": "/login-app/static/js/2.4e96a2a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/login-app/static/js/2.4e96a2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9754714cfc7c6101334a",
+    "url": "/login-app/static/js/main.8c3f6733.chunk.js"
+  },
+  {
+    "revision": "9d31a1c97ffa839210ed",
+    "url": "/login-app/static/js/runtime-main.102f41e8.js"
+  }
+]);
